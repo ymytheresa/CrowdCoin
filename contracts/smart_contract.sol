72455a5ae3f658388a591c190514370739b3c6be
Theresa <38038286+ymytheresa@users.noreply.github.com>
2021-03-31 01:39:31 +0800

    remove test contracts

--- contracts/smart_contract.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.6.6;
-contract SimpleContract {
-     uint value;
-     function setValue(uint _value) external {value = _value;}
-     function getValue() external view returns(uint){return value;}       
-}
